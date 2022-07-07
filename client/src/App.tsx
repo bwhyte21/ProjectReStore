df876a4ac785878ae60aff42ec8c5400129dc4eb
Bryan Whyte <bwhyte21@outlook.com>
2022-07-06 20:20:05 -0400

    Added useEffect hook.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   // We need to store what is inside this component to store this data in memory, using states.
@@ -6,6 +6,13 @@ function App() {
     { name: 'product1', price: 100.0 },
     { name: 'product2', price: 200.0 },
   ]);
+
+  // useEffect hooks allows us to add a side effect to a component when it loads.
+  useEffect(() => {
+    fetch('https://localhost:5001/api/Products')
+      .then((response) => response.json()) // the fetch will give us a json response.
+      .then((data) => setProducts(data)); // then we get data from said response.
+  }, []); // [] = empty dependency makes this useEffect get called only once. Remember this!
 
   // Add a new product to the list.
   // prevState grants access to what the product was before modification.
