852fdcc750d888350c8a59cd90336ca473e2e28f
Bryan Whyte <bwhyte21@outlook.com>
2022-07-06 20:04:46 -0400

    Added .prettierrc file. Added useState hook to skeleton project.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // We need to store what is inside this component to store this data in memory, using states.
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.0 },
+    { name: 'product2', price: 200.0 },
+  ]);
+
+  // Add a new product to the list.
+  // prevState grants access to what the product was before modification.
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      { name: 'product' + (prevState.length + 1), price: prevState.length * 100 + 100 },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={addProduct}>
+        Add Product
+      </button>
     </div>
   );
 }
